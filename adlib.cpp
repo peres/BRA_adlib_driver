b4365f07898b933d8ecdd864f6669c6aa6ca2d16
peres <peres@scummvm.org>
2011-02-20 01:39:29 +0900

    Correctly select bassdrum synthesis mode.

--- adlib.cpp
+++ adlib.cpp
@@ -882,19 +882,19 @@ void ADLIB_play_percussion() {
 		driver_percussion_mask &= ~(0x10);
 		ADLIB_out(0xBD, driver_percussion_mask);
 	
-		if (percussion_notes[midi_onoff_note].feedback_algo) {
-			// operator 2 is modulation operation 1	
-			uint8 scaling_level = percussion_notes[midi_onoff_note].levels_2;
-			uint8 total_level = TOTAL_LEVEL(midi_onoff_velocity, MAXIMUM_LEVEL);
-			ADLIB_out(0x53, ADLIB_40(scaling_level, total_level));
-		} else {
-			// operators 1 and 2 are independent
+		if (percussion_notes[midi_onoff_note].feedback_algo & 1) {
+			// operators 1 and 2 in additive synthesis
 			uint8 scaling_level = percussion_notes[midi_onoff_note].levels;
 			uint8 total_level = TOTAL_LEVEL(midi_onoff_velocity, MAXIMUM_LEVEL);
 			ADLIB_out(0x50, ADLIB_40(scaling_level, total_level));
 
 			scaling_level = percussion_notes[midi_onoff_note].levels_2;
 			total_level = TOTAL_LEVEL(midi_onoff_velocity, MAXIMUM_LEVEL);
+			ADLIB_out(0x53, ADLIB_40(scaling_level, total_level));
+		} else {
+			// operator 2 is modulating operator 1	
+			uint8 scaling_level = percussion_notes[midi_onoff_note].levels_2;
+			uint8 total_level = TOTAL_LEVEL(midi_onoff_velocity, MAXIMUM_LEVEL);
 			ADLIB_out(0x53, ADLIB_40(scaling_level, total_level));
 		}
 		
